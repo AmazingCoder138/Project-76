abe7483e502fa31e41e6c60413763f71f4577008
AmazingCoder138 <85464089+AmazingCoder138@users.noreply.github.com>
2021-11-28 18:32:34 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var firstname = "Priyanshi";
+var lastname = "Sapra";
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+var a = 7;
+var b = 11;
+var add = a + b;
+console.log(add);s
